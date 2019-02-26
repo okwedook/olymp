0f33bf731657ce432b04e43272225bf0036fdd42
Michael <pogodin2001@gmail.com>
2019-02-27 01:10:12 +0300

    Update Geometry.cpp

--- Geometry.cpp
+++ Geometry.cpp
@@ -1,34 +1,19 @@
 struct vec {
     ll x, y;
     vec() {}
-    vec(ll x, ll y) {
-        this->x = x;
-        this->y = y;
-    }
-    vec operator+(vec a) {
-        return vec(x + a.x, y + a.y);
-    }
-    vec operator-(vec a) {
-        return vec(x - a.x, y - a.y);
-    }
-    ll operator*(vec a) {
-        return x * a.x + y * a.y;
-    }
-    ll operator/(vec a) {
-        return x * a.y - y * a.x;
-    }
-    ll len2() {
-        return x * x + y * y;
-    }
-    ld len() {
-        return sqrt(len2());
-    }
-    friend istream& operator>>(istream& stream, vec &v) {
-        cin >> v.x >> v.y;
-        return stream;
-    }
-    friend ostream& operator<<(ostream& stream, vec v) {
-        cout << v.x << ' ' << v.y;
-        return stream;
-    }
+    vec(ll x, ll y) { this->x = x; this->y = y; }
+    vec operator+=(const vec &v) { x += v.x; y += v.y; return *this; }
+    vec operator-=(const vec &v) { x -= v.x; y -= v.y; return *this; }
+    vec operator*=(const ll &k) { x *= k; y *= k; return *this; }
+    vec operator-() const { return vec(-x, -y); }
+    vec orth() const { return vec(-y, x); }
+    ll len2() const { return x * x + y * y; }
+    ld len() const { return sqrt(len2()); }
+    friend vec operator+(vec a, const vec &b) { return a += b; }
+    friend vec operator-(vec a, const vec &b) { return a -= b; }
+    friend vec operator*(vec a, const ll &k) { return a *= k; }
+    friend ll operator*(const vec &a, const vec &b) { return a.x * b.x + a.y * b.y; }
+    friend ll operator/(const vec &a, const vec &b) { return a.x * b.y - a.y * b.x; }
+    friend istream& operator>>(istream& str, vec &v) { return str >> v.x >> v.y; }
+    friend ostream& operator<<(ostream& str, const vec &v) { return str << v.x << ' ' << v.y; }
 };
