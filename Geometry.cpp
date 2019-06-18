0412a58f56a343040d62c28825d3efc37f3132c1
Michael <pogodin2001@gmail.com>
2019-06-19 02:51:06 +0300

    Update Geometry.cpp

--- Geometry.cpp
+++ Geometry.cpp
@@ -1,22 +1,22 @@
-struct pos {
+struct vec {
     ll x, y;
-    pos() {}
-    pos(ll x, ll y) { this->x = x; this->y = y; }
-    pos operator+=(const pos &v) { x += v.x; y += v.y; return *this; }
-    pos operator-=(const pos &v) { x -= v.x; y -= v.y; return *this; }
-    pos operator*=(const ll &k) { x *= k; y *= k; return *this; }
-    pos operator-() const { return pos(-x, -y); }
-    pos orth() const { return pos(-y, x); }
+    vec() {}
+    vec(ll x, ll y) { this->x = x; this->y = y; }
+    vec operator+=(const vec &v) { x += v.x; y += v.y; return *this; }
+    vec operator-=(const vec &v) { x -= v.x; y -= v.y; return *this; }
+    vec operator*=(const ll &k) { x *= k; y *= k; return *this; }
+    vec operator-() const { return vec(-x, -y); }
+    vec orth() const { return vec(-y, x); }
     ll len2() const { return x * x + y * y; }
     ld len() const { return sqrt(len2()); }
-    friend pos operator+(pos a, const pos &b) { return a += b; }
-    friend pos operator-(pos a, const pos &b) { return a -= b; }
-    friend pos operator*(pos a, const ll &k) { return a *= k; }
-    friend ll operator*(const pos &a, const pos &b) { return a.x * b.x + a.y * b.y; }
-    friend ll operator/(const pos &a, const pos &b) { return a.x * b.y - a.y * b.x; }
-    friend istream& operator>>(istream& str, pos &v) { return str >> v.x >> v.y; }
-    friend ostream& operator<<(ostream& str, const pos &v) { return str << v.x << ' ' << v.y; }
-    friend bool operator<(const pos &a, const pos &b) { return a.x == b.x ? a.y < b.y : a.x < b.x; }
-    friend bool operator==(const pos &a, const pos &b) { return a.x == b.x && a.y == b.y; }
-    friend bool operator!=(const pos &a, const pos &b) { return a.x != b.x || a.y != b.y; }
+    friend vec operator+(vec a, const vec &b) { return a += b; }
+    friend vec operator-(vec a, const vec &b) { return a -= b; }
+    friend vec operator*(vec a, const ll &k) { return a *= k; }
+    friend ll operator*(const vec &a, const vec &b) { return a.x * b.x + a.y * b.y; }
+    friend ll operator/(const vec &a, const vec &b) { return a.x * b.y - a.y * b.x; }
+    friend istream& operator>>(istream& str, vec &v) { return str >> v.x >> v.y; }
+    friend ostream& operator<<(ostream& str, const vec &v) { return str << v.x << ' ' << v.y; }
+    friend bool operator<(const vec &a, const vec &b) { return a.x == b.x ? a.y < b.y : a.x < b.x; }
+    friend bool operator==(const vec &a, const vec &b) { return a.x == b.x && a.y == b.y; }
+    friend bool operator!=(const vec &a, const vec &b) { return a.x != b.x || a.y != b.y; }
 };
