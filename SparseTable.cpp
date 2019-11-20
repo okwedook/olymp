3590d7490aedb2b72607513879adcf2a83cb7b49
Michael <pogodin2001@gmail.com>
2019-11-20 21:26:43 +0300

    Update SparseTable.cpp

--- SparseTable.cpp
+++ SparseTable.cpp
@@ -1,18 +1,23 @@
-template<class T> struct sparse {
-    static const int LOG = 18;
-    static const int N = 2e5 + 10;
-    int mx2[N];
-    T v[LOG][N];
-    sparse(vector<T> a) {
-        for (int i = 2; i < N; ++i) mx2[i] = mx2[i >> 1] + 1;
-        int n = sz(a);
-        for (int i = 0; i < n; ++i) v[0][i] = a[i];
-        for (int l = 1; l < LOG; ++l) 
-            for (int i = 0; i + (1 << l) <= n; ++i)
-                v[l][i] = min(v[l - 1][i], v[l - 1][i + (1 << l - 1)]);
+template<class T, const T& (*cmp)(const T&, const T&) = min>
+struct sparse {
+    int N, LOG;
+    vector<int> mx;
+    vector<vector<T>> val;
+    //array a must not be empty
+    sparse(const vector<T> &a) {
+        N = sz(a), LOG = ceil(log2(N));
+        mx = vector<int>(N + 1);
+        for (int i = 2; i < sz(mx); ++i) mx[i] = mx[i >> 1] + 1;
+        val = vector<vector<T>>(LOG, vector<T>(N));
+        for (int i = 0; i < N; ++i) val[0][i] = a[i];
+        for (int l = 1; l < LOG; ++l)
+            for (int i = 0, next = 1 << (l - 1); next < N; ++i, ++next)
+                val[l][i] = cmp(val[l - 1][i], val[l - 1][next]);
     }
+    //[l, r] range query
     T get(int l, int r) {
-        int mx = mx2[r - l + 1];
-        return min(v[mx][l], v[mx][r - (1 << mx) + 1]);
+        ++r;
+        int len = mx[r - l];
+        return cmp(val[len][l], val[len][r - (1 << len)]);
     }
 };
