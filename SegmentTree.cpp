3575dc72c61b42f1ca005578af899382a3a807d4
Michael <pogodin2001@gmail.com>
2019-12-07 03:35:05 +0300

    Update SegmentTree.cpp

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -1,74 +1,166 @@
+#pragma GCC optimize("O2", "unroll-all-loops")
+//#pragma GCC target("avx2")
+ 
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <bitset>
+#include <sstream>
+#include <deque>
+#include <queue>
+#include <random>
+#include <cassert>
+#include <complex>
+ 
+using namespace std;
+ 
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define FIXED cout << fixed << setprecision(12)
+#define RANDOM srand(94385)
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define graph vector<vector<int>>
+#define pb push_back
+#define pf push_front
+#define popb pop_back
+#define popf pop_front
+#define f first
+#define s second
+#define hashmap unordered_map
+#define hashset unordered_set
+#define eps 1e-9
+#define mod 998244353
+#define inf 3000000000000000007ll
+#define sz(a) signed(a.size())
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+ 
+template<class T, class U> inline void checkmin(T &x, U y) { if (y < x) x = y; }
+template<class T, class U> inline void checkmax(T &x, U y) { if (y > x) x = y; }
+template<class T, class U> inline bool ifmax(T &x, U y) { if (y > x) return x = y, true; return false; }
+template<class T, class U> inline bool ifmin(T &x, U y) { if (y < x) return x = y, true; return false; }
+template<class T> inline void sort(T &a) { sort(all(a)); }
+template<class T> inline void rsort(T &a) { sort(rall(a)); }
+template<class T> inline void reverse(T &a) { reverse(all(a)); }
+template<class T, class U> inline istream& operator>>(istream& str, pair<T, U> &p) { return str >> p.f >> p.s; }
+template<class T> inline istream& operator>>(istream& str, vector<T> &a) { for (auto &i : a) str >> i; return str; }
+template<class T> inline T sorted(T a) { sort(a); return a; }
+
 struct node {
-	int mn, mx;
-	node() : mn(mod), mx(-mod) {}
-	node(int x) : mn(x), mx(x) {}
-	node(int _mn, int _mx) : mn(_mn), mx(_mx) {}
-	int get() const {
-		return mx - mn;
-	}
-};
- 
-inline node merge(const node &a, const node &b) {
-    return node(min(a.mn, b.mn), max(a.mx, b.mx));
-}
- 
-template<class T> inline void merge(const T &a, const T &b, T &c) {
-	c = move(merge(a, b));
-}
- 
-template<class node>
-class Tree {
-private:
-    node *t = nullptr;
-    int n;
- 
-public:
-    ~Tree() {
-        delete [] t;
+    int val;
+    bool flag = false;
+    node() {}
+    node(int x) : val(x) {}
+    void upd(int x) {
+        val = x;
+        flag = true;
     }
-    void resize(int sz) {
-    	n = 1;
-    	while (sz > n) n *= 2;
-    	t = new node[2 * n + 1];
+    void upd(const node &t) {
+        upd(t.val);
+    }
+    bool updated() {
+        return flag;
+    }
+    void unupdate() {
+        flag = false;
+    }
+};
+
+node merge(const node &a, const node &b) {
+    node ans;
+    ans.val = max(a.val, b.val);
+    return ans;
+}
+
+template<class node, node (*merge)(const node&, const node&) = merge>
+struct Tree {
+    int n = 1;
+    node *t = nullptr;
+    void submerge(int v) { // merge v's sons to v
+        if (v >= n) return;
+        t[v] = merge(t[v + v], t[v + v + 1]);
+    }
+    void resize(int sz) { // allocation
+        while (n < sz) n <<= 1;
+        delete [] t;
+        t = new node[2 * n + 1];
+    }
+    void blank() { // default values
+        for (int i = 0; i < 2 * n; ++i)
+            t[i] = node();
     }
     template<class T>
-    void assign(const vector<T> &a) {
-    	resize(sz(a));
+    void assign(const vector<T> &a) { // copy of a
+        resize(sz(a));
         for (int i = 0; i < sz(a); ++i)
             t[i + n] = a[i];
         for (int i = sz(a); i < n; ++i)
-        	t[i + n] = node();
+            t[i + n] = node();
         for (int i = n - 1; i > 0; --i)
-            t[i] = merge(t[i + i], t[i + i + 1]);
+            submerge(i);
     }
+    ~Tree() { delete [] t; }
+    Tree() {} // empty
+    Tree(int sz) { resize(sz); blank(); } // size, default values
+    template<class T> Tree(const vector<T> &a) { assign(a); } // copy of a
     template<class T>
-    Tree(const vector<T> &a) {
-    	assign(a);
+    void updup(int i, const T &val) { // upd at i with val, doesn't support push
+        i += n;
+        t[i] = val;
+        for (i >>= 1; i > 0; i >>= 1)
+            submerge(i);
     }
-    Tree(int sz, node x = node()) {
-    	resize(sz);
-    	for (int i = 0; i < n; ++i)
-    		t[i + n] = x;
-    	for (int i = n - 1; i >= 0; --i)
-    		t[i] = merge(t[i + i], t[i + i + 1]);
-    }
-    void updup(int r, node x) { //upd r-th element with x (w/o push)
-        r += n;
-        t[r] = x;
-        for (r >>= 1; r > 1; r >>= 1)
-            t[r] = merge(t[r + r], t[r + r + 1]);
-    }
-    node getup(int l, int r) { //get on [l, r] (w/o push)
+    node getup(int l, int r) { // get at [l, r), doesn't support push
         l += n;
-        r += n + 1;
-        node left = node(), right = node();
+        r += n;
+        int cr = r;
+        node left, right;
+        bool lf = true, rf = true;
         for (; l < r; l >>= 1, r >>= 1) {
-            if (l & 1) left = merge(left, t[l++]);
-            if (r & 1) right = merge(t[--r], right);
+            if (l & 1) left = lf ? lf = false, t[l++] : merge(left, t[l++]);
+            if (r & 1) right = rf ? rf = false, t[--r] : merge(t[--r], right);
         }
+        if (lf) return right;
+        if (rf) return left;
         return merge(left, right);
     }
-    node get(int l, int r) {
-    	return t[0].push();
+    void push(int v) {
+        if (t[v].updated()) {
+            t[v + v].upd(t[v]);
+            t[v + v + 1].upd(t[v]);
+            t[v].unupdate();
+        }
     }
+    template<class T>
+    void upd(int l, int r, const T &x, int v, int vl, int vr) {
+        if (vl >= r || vr <= l) return;
+        if (vl >= l && vr <= r) return t[v].upd(x);
+        push(v);
+        int vm = vl + vr >> 1;
+        upd(l, r, x, v + v, vl, vm);
+        upd(l, r, x, v + v + 1, vm, vr);
+        submerge(v);
+    }
+    template<class T>
+    void upd(int l, int r, const T &x) { upd(l, r, x, 1, 0, n); } 
+    
+    node get(int l, int r, int v, int vl, int vr) {
+        if (vl >= r || vr <= l) return node();
+        if (vl >= l && vr <= r) return t[v];
+        push(v);
+        int vm = vl + vr >> 1;
+        if (r <= vm) return get(l, r, v + v, vl, vm);
+        if (l >= vm) return get(l, r, v + v + 1, vm, vr);
+        return merge(get(l, r, v + v, vl, vm), get(l, r, v + v + 1, vm, vr));
+    }
+    node get(int l, int r) { return get(l, r, 1, 0, n); } 
 };
