91d1e884cd00f0b3039d2705d6e1200fc0ca3794
Michael <okwedook@gmail.com>
2020-01-07 21:12:29 +0300

    Update BigNumsKaratsuba.cpp

--- BigNumsKaratsuba.cpp
+++ BigNumsKaratsuba.cpp
@@ -1,198 +1,140 @@
-const int base = 1000000000;
-const int digits = 9;
-
-struct num {
-    vector<int> a;
+struct num : vector<int> {
+    static const int base = 1e9;
+    static const int dig = 9;
     bool minus = false;
     num() {}
-    num(int x) {
-        if (x < 0) x = -x, minus = true;
-        while (x > 0) a.pb(x % base), x /= base;
-    }
-    num(ll x) {
-        if (x < 0) x = -x, minus = true;
-        while (x > 0) a.pb(x % base), x /= base;
-    }
-    num(string s) {
-        if (s[0] == '-') {
-            minus = true;
-            s = s.substr(1, sz(s) - 1);
+    num(int x) { if (x < 0) minus = true, x = -x; while (x > 0) pb(x % base), x /= base; }
+    num(ll x) { if (x < 0) minus = true, x = -x; while (x > 0) pb(x % base), x /= base; }
+    template<class It> num(It begin, It end) { while (begin < end) pb(*begin++); }
+    inline int& get(int ind) { return (*this)[ind]; }
+    inline int get(int ind) const { return (*this)[ind]; }
+    inline void clr() { while (!empty() && back() == 0) popb(); if (empty()) minus = false; }
+    friend string to_string(const num &x) {
+        string ans;
+        if (x.minus) ans += '-';
+        ans += (sz(x) == 0 ? "0" : to_string(x.back()));
+        for (int i = sz(x) - 2; i >= 0; --i) {
+            string add = to_string(x[i]);
+            for (int j = sz(add); j < dig; ++j) ans += '0';
+            ans += add;
         }
-        for (int i = sz(s) - digits; i > 0; i -= digits)
-            pb(stoi(s.substr(i, digits)));
-        pb(stoi(s.substr(0, (sz(s) - 1) % digits + 1)));
-        clear();
+        return ans;
     }
-    template<class T> inline int& operator[](T ind) { return a[ind]; }
-    template<class T> inline int operator[](T ind) const { return a[ind]; }
-    inline size_t size() const { return a.size(); }
-    inline int back() const { return a.back(); }
-    template<class T> inline void pb(T x) { a.pb(x); }
-    inline num operator-() const {
+    bool cmpabs(const num &x) const { //abs(this) < abs(x)
+        if (size() != sz(x)) return size() < sz(x);
+        for (int i = sz(x) - 1; i >= 0; --i)
+            if (get(i) != x[i]) return get(i) < x[i];
+        return false;
+    }
+    num operator-() const {
         num ans = *this;
         ans.minus ^= true;
         return ans;
     }
-    inline num abs() const {
-        num ans = *this;
-        ans.minus = false;
-        return ans;
-    }
-    inline void clear() {
-        while (sz(a) && a.back() == 0) a.popb();
-        if (sz(a) == 0) minus = false;
-    }
-    inline bool cmpabs(const num &v) const {
-        if (sz(a) != sz(v)) return sz(a) < sz(v);
-        for (int i = sz(a) - 1; i >= 0; --i)
-            if (a[i] != v[i]) return a[i] < v[i];
-        return false;
-    }
-    inline bool operator==(const num &v) const {
-        return minus == v.minus && a == v.a;
-    }
-    inline bool operator<(const num &v) const {
-        if (minus != v.minus) return minus;
-        return cmpabs(v);
-    }
-    inline bool operator<=(const num &v) const { return !(v < *this); }
-    inline bool operator>(const num &v) const { return v < *this; }
-    inline bool operator>=(const num &v) const { return !(v > *this); }
-
-    inline num operator+(const num& v) const {
-        if (minus != v.minus) return *this - v;
-        num ans = *this;
-        for (int i = 0, cur = 0; i < sz(v) || cur; ++i) {
-            if (i == sz(ans)) ans.pb(0);
-            ans[i] += (i < sz(v) ? v[i] : 0) + cur;
-            cur = ans[i] >= base;
-            if (cur) ans[i] -= base;
+    num& operator+=(const num &x) {
+        if (minus != x.minus) return *this -= (-x);
+        for (int i = 0, cur = 0; i < sz(x) || cur; ++i) {
+            if (i == size()) pb(0);
+            if (i < sz(x)) get(i) += x[i];
+            get(i) += cur;
+            cur = get(i) >= base;
+            if (cur) get(i) -= base;
         }
-        ans.clear();
-        return ans;
+        return *this;
     }
-    inline num operator-(const num &v) const {
-        if (minus != v.minus) return *this + (-v);
-        if (cmpabs(v)) return -(v - *this);
-        num ans = *this;
-        for (int i = 0, cur = 0; i < sz(v) || cur; ++i) {
-            if (i == sz(ans)) ans.pb(0);
-            ans[i] -= (i < sz(v) ? v[i] : 0) + cur;
-            cur = ans[i] < 0;
-            if (cur) ans[i] += base;
+    num& operator-=(const num &x) {
+        if (minus != x.minus) return *this += (-x);
+        if (cmpabs(x)) return *this = -(x - *this);
+        for (int i = 0, cur = 0; i < sz(x) || cur; ++i) {
+            if (i == size()) pb(0);
+            if (i < sz(x)) get(i) -= x[i];
+            get(i) -= cur;
+            cur = get(i) < 0;
+            if (cur) get(i) += base;
         }
-        ans.clear();
-        return ans;
+        clr();
+        return *this;
     }
-
-
-    inline num cut(int l, int r) const {
+    num powbase(int p) const {
         num ans;
-        for (int i = l; i < r; ++i)
-            ans.pb(a[i]);
+        for (int i = 0; i < p; ++i) ans.pb(0);
+        for (auto i : *this) ans.pb(i);
         return ans;
     }
-
-    inline num powbase(int x) const {
-        num ans;
-        for (int i = 0; i < x; ++i) ans.pb(0);
-        for (auto i : a) ans.pb(i);
-        return ans;
+    num cut(int l, int r) const {
+        return num(begin() + l, begin() + r);
     }
-
-    inline num operator*(const num &v) const {
-        num a = *this, b = v;
+    num multiply(num a, num b) {
+        if (sz(a) == 0) return 0;
         while (sz(a) < sz(b)) a.pb(0);
-        while (sz(a) > sz(b)) b.pb(0);
-        while (__builtin_popcount(sz(a)) != 1) a.pb(0), b.pb(0);
+        while (sz(b) < sz(a)) b.pb(0);
         if (sz(a) == 1) return a[0] * (ll)b[0];
-        int n = sz(a);
-        int k = n >> 1;
-        num a1 = a.cut(0, k), a2 = a.cut(k, n);
-        num b1 = b.cut(0, k), b2 = b.cut(k, n);
-        num z0 = a1 * b1, z1 = a2 * b2, z2 = (a1 + a2) * (b1 + b2);
-        num ans = z0 + (z2 - z0 - z1).powbase(n - k) + z1.powbase(n);
-        ans.minus = a.minus ^ b.minus;
-        ans.clear();
-        return ans;
+        int m = sz(a) / 2;
+        num a1 = a.cut(0, m), a2 = a.cut(m, sz(a)),
+            b1 = b.cut(0, m), b2 = b.cut(m, sz(b));
+        num x1 = multiply(a1, b1), x2 = multiply(a2, b2);
+        num x3 = multiply(a1 + a2, b1 + b2);
+        x1.clr();
+        x2.clr();
+        x3 -= x1;
+        x3 -= x2;
+        return x2.powbase(m << 1) + x3.powbase(m) + x1;
+    }
+    num& operator*=(const num &x) {
+        bool flag = minus ^ x.minus;
+        *this = multiply(*this, x);
+        minus = flag;
+        clr();
+        return *this;
+    }
+    num& operator/=(const num &x) {
+        
     }
 
-    inline num div2() const {
-        num ans = *this;
-        for (int i = sz(ans) - 1, cur = 0; i >= 0; --i) {
-            ans[i] += cur;
-            cur = ans[i] & 1;
-            ans[i] /= 2;
-            if (cur) cur = base;
-        }
-        return ans;
+    bool operator<(const num &x) const {
+        if (minus != x.minus) return minus;
+        return cmpabs(x) ^ minus;
+    }
+    bool operator>(const num &x) const {
+        return x < *this;
+    }
+    bool operator<=(const num &x) const {
+        return !(*this > x);
+    }
+    bool operator>=(const num &x) {
+        return x <= *this;
+    }
+    bool operator!=(const num &x) {
+        return !(*this == x);
     }
 
-    inline num operator/(const num &v) const {
-        num l = 0, r = abs() + 1;
-        while (r - l > 1) {
-            num m = (r + l).div2();
-            if (*this >= v * m) l = m;
-            else r = m;
-        }
-        l.minus = minus;
-        l.clear();
-        return l;
-    }
-
-    inline void operator+=(const num &v) {
-        *this = *this + v;
-    }
-
-    inline void operator-=(const num &v) {
-        *this = *this - v;
-    }
-
-    inline void operator*=(const num &v) {
-        *this = *this * v;
-    }
-
-    inline void operator/=(const num &v) {
-        *this = *this / v;
-    }
-
-    inline string strval() const {
-        string s = "";
-        if (minus) s.pb('-');
-        if (sz(a)) s += to_string(back());
-        else s.pb('0');
-        for (int i = sz(a) - 2; i >= 0; --i) {
-            string news = to_string(a[i]);
-            for (int j = sz(news); j < digits; ++j)
-                s.pb('0');
-            s += news;
-        } 
-        return s;
-    }
-    friend istream& operator>>(istream& stream, num &v) {
+    friend istream& operator>>(istream& str, num &x) {
         string s;
-        stream >> s;
-        v = num(s);
-        return stream;
+        str >> s;
+        x.minus = false;
+        x.clear();
+        if (s[0] == '-') x.minus = true, s.erase(s.begin());
+        for (int i = sz(s) - dig; i > 0; i -= dig)
+            x.pb(stoi(s.substr(i, dig)));
+        x.pb(stoi(s.substr(0, (sz(s) - 1) % dig + 1)));
+        return str;
     }
-    friend ostream& operator<<(ostream& stream, const num &v) {
-        if (v.minus) stream << '-';
-        if (sz(v) == 0) stream << '0';
-        else stream << v.back();
-        for (int i = sz(v) - 2; i >= 0; --i) 
-            stream << setw(digits) << setfill('0') << v[i];
-        return stream;
+    friend ostream& operator<<(ostream& str, const num &x) {
+        return str << to_string(x);
     }
-    friend num sqrt(const num &v) {
-        num l = 0, r = v;
-        while (r - l > 1) {
-            num m = (r + l).div2();
-            if (m * m > v) r = m;
-            else l = m;
+
+    friend num mypow(num a, int n) {
+        num ans = 1;
+        while (n > 0) {
+            if (n & 1) ans *= a;
+            a *= a;
+            n >>= 1;
         }
-        return l;
+        return ans;
     }
-    friend string to_string(const num &v) {
-        return v.strval();
-    }
+
+    friend num operator+(num a, const num &b) { return a += b; }
+    friend num operator-(num a, const num &b) { return a -= b; }
+    friend num operator*(num a, const num &b) { return a *= b; }
+    friend num operator/(num a, const num &b) { return a /= b; }
 };
