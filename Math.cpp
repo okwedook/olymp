29417a15da9ba9868f910e0bd0694fef10667549
Michael <okwedook@gmail.com>
2020-04-01 05:20:20 +0300

    Update Math.cpp

--- Math.cpp
+++ Math.cpp
@@ -16,4 +16,37 @@ namespace math {
             if (used[i]) ans.pb(2 * i + 1);
         return ans;
     }
+    vector<int> mindiv(int n) {
+        vector<int> ans(n + 1);
+        for (int i = 2; i * i <= n; ++i)
+            if (ans[i] == 0)
+                for (int j = i * i; j <= n; j += i)
+                    if (ans[j] == 0) ans[j] = i;
+        for (int i = 1; i <= n; ++i)
+            if (ans[i] == 0) ans[i] = i;
+        return ans;
+    }
+    vector<int> dec(int x) {
+        static const int MXNUMS = 1000000;
+        static auto mn = mindiv(MXNUMS);
+        vector<int> ans;
+        while (x > 1) {
+            ans.pb(mn[x]);
+            x /= mn[x];
+        }
+        return ans;
+    }
+    vector<int> decbig(int x) {
+        static const int MXNUMS = 2000000000;
+        static auto pr = genprimes(int(sqrt(MXNUMS)) + 10);
+        vector<int> ans;
+        for (auto i : pr) {
+            while (x % i == 0) {
+                ans.pb(i);
+                x /= i;
+            }
+        }
+        if (x > 1) ans.pb(x);
+        return ans;
+    }
 };
