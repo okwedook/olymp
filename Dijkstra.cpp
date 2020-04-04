3628cb8d0aa60a10636962635ef6d88d61b1f087
Michael <okwedook@gmail.com>
2020-04-05 02:57:36 +0300

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,16 +1,12 @@
-typedef ll F;
-vector<F> dijkstra(vector<vector<pii>> &G, int v) {
-    vector<F> ans(sz(G), inf);
+vector<ll> dijkstra(vector<vector<pii>> &G, int v) {
+    vector<ll> ans(sz(G), inf);
     ans[v] = 0;
     auto cmp = [&](int i, int j) { return ans[i] > ans[j]; };
     priority_queue<int, vector<int>, decltype(cmp)> que(cmp);
     que.push(v);
-    vector<bool> used(sz(G));
     while (!que.empty()) {
         auto f = que.top();
         que.pop();
-        if (used[f]) continue;
-        used[f] = true;
         for (auto i : G[f])
             if (ifmin(ans[i.f], ans[f] + i.s))
                 que.push(i.f);
