01d6c2a222cf4c118f6488927e25bb0f5e5fbcd6
Michael <okwedook@gmail.com>
2020-06-18 22:06:25 +0300

    Update Dijkstra.cpp
    Major bug-fix

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -3,13 +3,18 @@ vector<ll> dijkstra(vector<vector<pii>> &G, int v) {
     ans[v] = 0;
     auto cmp = [&](int i, int j) { return ans[i] > ans[j]; };
     priority_queue<int, vector<int>, decltype(cmp)> que(cmp);
+    vector<bool> flag(sz(G));
     que.push(v);
     while (!que.empty()) {
         auto f = que.top();
         que.pop();
+        if (flag[f]) continue;
+        flag[f] = true;
         for (auto i : G[f])
-            if (chmin(ans[i.f], ans[f] + i.s))
+            if (chmin(ans[i.f], ans[f] + i.s)) {
                 que.push(i.f);
+                flag[f] = false;
+            }
     }
     return ans;
 }
