dd1d8eaffef844ecd1399decea47adeeb3b4acf0
Michael <okwedook@gmail.com>
2020-10-10 00:17:36 +0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -91,10 +91,12 @@ template<class T> inline void reverse(T &a) { reverse(all(a)); }
 template<class T, class U> inline istream& operator>>(istream& str, pair<T, U> &p) { return str >> p.f >> p.s; }
 template<class T> inline istream& operator>>(istream& str, vector<T> &a) { for (auto &i : a) str >> i; return str; }
 template<class T> inline T sorted(T a) { sort(a); return a; }
-void read() {} void print() {} void println() { cout << '\n'; }
-template<class T, class ...U> void read(T &x, U& ... u) { cin >> x; read(u...); }
-template<class T, class ...U> void print(const T &x, const U& ... u) { cout << x; print(u...); }
-template<class T, class ...U> void println(const T &x, const U& ... u) { cout << x; println(u...); }
+void println() { cout << '\n'; }
+template<class T> void print(const T &x) { cout << x; }
+template<class T> void read(T &x) { cin >> x; }
+template<class T, class ...U> void read(T &x, U& ... u) { read(x); read(u...); }
+template<class T, class ...U> void print(const T &x, const U& ... u) { print(x); print(u...); }
+template<class T, class ...U> void println(const T &x, const U& ... u) { print(x); println(u...); }
 
 signed main() {
     FAST; FIXED;
