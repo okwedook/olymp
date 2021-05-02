f3dc064ca4451f7a3db6c983d9736d41a27c9538
Michael <okwedook@gmail.com>
2021-05-03 00:25:39 +0300

    Update BIT.cpp

--- data-structures/BIT.cpp
+++ data-structures/BIT.cpp
@@ -1,32 +1,54 @@
-template<class T> struct Fenv {
-    int N, LOG;
-    T *fenv;
-    Fenv(int N) {
-        this->N = N;
-        LOG = log2(N);
-        fenv = new T[N];
-        for (int i = 0; i < N; ++i) fenv[i] = 0;
+/*
+* Author: Mikhail Pogodin (okwedook)
+*
+* Fenwick tree implementation
+* Uses linear space and O(logn) time per operation.
+*
+* Check tasks: https://codeforces.com/gym/100246/problem/A
+*/
+
+template<class T>
+struct Fenwick {
+    vector<T> sum;
+    Fenwick() {}
+    Fenwick(int n) { sum.resize(n); }
+    // Initializes Fenwick tree by given values in linear time
+    template<class U>
+    Fenwick(const U &a) {
+        sum.assign(all(a));
+        build();
     }
-	~Fenv() { delete [] fenv; }
-    void add(int r, T x) { //adds x to a[r]
-        for (; r < N; r |= r + 1) fenv[r] += x;
+    // Makes values into fenwick values
+    void build() {
+        for (int r = 0; r < sz(sum); ++r)
+            if ((r | r + 1) < sz(sum))
+                sum[r | r + 1] += sum[r];
     }
-    T get(int r) { //sum of [0, r]
+    // Adds x to position r
+    void add(int r, T x) {
+        for (; r < sz(sum); r |= r + 1)
+            sum[r] += x;
+    }
+    // Sum at [0, r]
+    T get(int r) {
         T ans = 0;
-        for (; r >= 0; r = (r & r + 1) - 1) 
-            ans += fenv[r];
+        for (; r >= 0; r = (r & r + 1) - 1)
+            ans += sum[r];
         return ans;
     }
-    T get(int l, int r) { //sum of [l, r]
+    // Sum at [l, r]
+    T get(int l, int r) {
         return get(r) - get(l - 1);
     }
-    int lb(T k) { //first index which sum is >= k
+    // First index, which sum at [0, ans] is at least x
+    int lb(T x) {
         int ans = -1;
-        for (int l = LOG; l >= 0; --l) {
+        ll now = 0;
+        for (int l = 31 - __builtin_clz(sz(sum)); l >= 0; --l) {
             int next = ans + (1 << l);
-            if (next < N && k > fenv[next]) {
+            if (next < sz(sum) && now + sum[next] < x) {
                 ans = next;
-                k -= fenv[next];
+                now += sum[next];
             }
         }
         return ans + 1;
