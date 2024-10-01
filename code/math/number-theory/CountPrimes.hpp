9cd1757619a5a47bcd1b0b012c444ce804660baa
Mikhail Pogodin <m.pogodin@vk.team>
2024-10-01 05:05:46 +0300

    :racehorse: :heavy_check_mark: Drastically improve count_primes performance

--- code/math/number-theory/CountPrimes.hpp
+++ code/math/number-theory/CountPrimes.hpp
@@ -1,29 +1,29 @@
-#include "Math.hpp"
-
 // Counts the primes in O(n^(3/4)) for n and
 // every number floor(n / k), where k is an int
 ll count_primes(ll n) {
-    ll x = 1;
     vector<ll> val;
+    val.reserve(2 * sqrt(n) + 2);
+    ll x = 1;
     while (x * x <= n) {
         val.pb(x);
         ++x;
     }
-    auto primes = math::genprimes(x - 1);
+    ll sq = x - 1;
     while (x > 0 && n / x <= val.back()) --x;
     while (x > 0) {
         val.pb(n / x);
         --x;
     }
+    auto get_ind = [sq, n, &val](ll x) {
+        return x <= sq ? x - 1 : sz(val) - (n / x);
+    };
     vector<ll> dp = val;
-    for (int a = 1; a <= sz(primes); ++a) {
-        int p = primes[a - 1];
-        for (int i = sz(dp) - 1, ptr = sz(dp) - 1; val[i] >= p * ll(p); --i) {
-            ll need = val[i] / p;
-            while (val[ptr] > need) {
-                --ptr;
-            }
-            dp[i] -= max(0ll, dp[ptr] - a);
+    int a = 0;
+    for (ll p = 2; p * p <= n; ++p) {
+        if (dp[p - 1] == dp[p - 2]) continue;
+        ++a;
+        for (int i = sz(dp) - 1; val[i] >= p * p; --i) {
+            dp[i] -= dp[get_ind(val[i] / p)] - a;
         }
     }
     return dp.back() - 1;
