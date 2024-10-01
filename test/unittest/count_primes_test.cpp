48316c2d288d7b3ad4774611fc54f0e8a3cc9c0f
Mikhail Pogodin <m.pogodin@vk.team>
2024-10-01 05:05:46 +0300

    :racehorse: :heavy_check_mark: Drastically improve count_primes performance

--- test/unittest/count_primes_test.cpp
+++ test/unittest/count_primes_test.cpp
@@ -1,4 +1,5 @@
 #include <code/general/base_template.hpp>
+#include "code/math/number-theory/Math.hpp"
 #include "code/math/number-theory/CountPrimes.hpp"
 
 #include <gtest/gtest.h>
@@ -10,6 +11,10 @@ TEST(CountPrimes, SmallTest) {
 }
 
 TEST(CountPrimes, BigTest) {
-    ASSERT_EQ(count_primes(10 * 1000ll * 1000ll * 1000ll), 455052511);
-    ASSERT_EQ(count_primes(1000 * 1000 * 1000 + 7), 50847535);
+    static const ll thousand = 1000;
+    static const ll billion = thousand * thousand * thousand;
+    ASSERT_EQ(count_primes(billion + 7), 50847535);
+    ASSERT_EQ(count_primes(10 * billion), 455052511);
+    ASSERT_EQ(count_primes(100 * billion), 4118054813ll);
+    ASSERT_EQ(count_primes(1000 * billion), 37607912018ll);
 }
